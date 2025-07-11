85e4631b265c9ec9adcd709188aa8eddfdd242f6
Ayush Singh <ayush0f@Ayushs-MacBook-Air.local>
2025-07-11 12:48:04 +0530

    changes are made

--- app.py
+++ app.py
@@ -5,21 +5,20 @@ from datetime import datetime
 app = Flask(__name__)
 
 # MongoDB connection
-
 client = MongoClient(
-    "mongodb+srv://mickey919051:2moB3FXdAfixj4dj@ayush.uheu9rs.mongodb.net/?retryWrites=true&w=majority&appName=Ayush")
+    "mongodb+srv://mickey919051:2moB3FXdAfixj4dj@ayush.uheu9rs.mongodb.net/?retryWrites=true&w=majority&appName=Ayush"
+)
 db = client.webhooks
 collection = db.events
-print(client.list_database_names())
 
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    print("tmkccccccccccccc")
+    print("Webhook received")
     data = request.json
     event = request.headers.get('X-GitHub-Event')
 
-    # defining all the vars
+    # define all vars
     author = action = from_branch = to_branch = request_id = timestamp = None
 
     if event == "push":
@@ -39,7 +38,6 @@ def webhook():
         action = data['action'].upper()
         if action == "CLOSED" and pr.get('merged'):
             action = "MERGE"
-
     else:
         print("Unhandled event:", event)
         return "Ignored", 200
@@ -53,26 +51,22 @@ def webhook():
         "timestamp": timestamp
     }
 
-    # Insert into MongoDB
+    # Save to MongoDB
     result = collection.insert_one(doc)
     print(f"Saved to MongoDB: {result.inserted_id}")
-
     return "Stored in MongoDB", 200
 
 
 @app.route('/')
 def index():
+    # looks in templates/index.html automatically
     return render_template('index.html')
-
-
-def get_events():
-    data = list(collection.find({}, {'_id': 0}).sort('timestamp', -1))
-    return jsonify(data)
 
 
 @app.route('/events')
 def events():
-    return get_events()
+    data = list(collection.find({}, {'_id': 0}).sort('timestamp', -1))
+    return jsonify(data)
 
 
 if __name__ == '__main__':
